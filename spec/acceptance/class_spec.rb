41538cebef5cee4e07d17a72c3d72851df267586
Trey Dockendorf <tdockendorf@osc.edu>
2019-05-08 15:13:36 -0400

    rubocop autocorrect

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper_acceptance'
 
 describe 'singularity class:' do
   context 'default parameters' do
-    it 'should run successfully' do
-      pp =<<-EOS
+    it 'runs successfully' do
+      pp = <<-EOS
       class { 'singularity': }
       EOS
 
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
     end
 
     describe package('singularity') do
-      it { should be_installed }
+      it { is_expected.to be_installed }
     end
 
     describe file('/etc/singularity/singularity.conf') do
-      it { should be_file }
-      it { should be_mode 644 }
-      it { should be_owned_by 'root' }
-      it { should be_grouped_into 'root' }
+      it { is_expected.to be_file }
+      it { is_expected.to be_mode 644 }
+      it { is_expected.to be_owned_by 'root' }
+      it { is_expected.to be_grouped_into 'root' }
     end
   end
 end
